11b16aaf96c3bcfb3aa8e056320738b512223731
SijiChen <chensiji19891@gmail.com>
2016-08-23 09:53:48 -0400

    add method for insert row, select by id, selectAll, used namedparameterJdbcTemplate

--- src/main/java/com/spring/persistence/dao/CustomerProfileDao.java
+++ src/main/java/com/spring/persistence/dao/CustomerProfileDao.java
@@ -5,26 +5,81 @@ import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by sjchen on 8/22/16.
  */
 
-public class CustomerProfileDao extends JdbcDaoSupport {
+public class CustomerProfileDao extends NamedParameterJdbcDaoSupport {
 
+    /**
+     * select row by id
+     * @param id
+     * @return customer_profile with id and email;
+     */
     public CustomerProfile customerProfile(Long id) {
 
-        final String SELECT = "select customer_profile_id as id, email as email from customer_profile where customer_profile_id = ?";
-
-        CustomerProfile profile = getJdbcTemplate().queryForObject(SELECT, new Object[]{id},
+        final String SELECT = "select customer_profile_id as id, email as email from " +
+                "customer_profile where customer_profile_id = :contactId";
+        Map<String, Object> namedParameters = new HashMap<String, Object>();
+        namedParameters.put("contactId", id);
+        CustomerProfile profile = getNamedParameterJdbcTemplate().queryForObject(SELECT, namedParameters,
                 new BeanPropertyRowMapper<CustomerProfile>(CustomerProfile.class));
-
         return profile;
 
     }
-    @Autowired
-    private static CustomerProfileDao customerProfileDao;
 
+    /**
+     * select email by id
+     * @param id
+     * @return string
+     */
+    public String findEmailById(Long id) {
+        return getJdbcTemplate().queryForObject(
+                "select email from customer_profile where  customer_profile_id = ?", new Object[]{id}, String.class);
+    }
+
+    /**
+     * insert a new row into table customer_profile
+     * @param id
+     * @param email
+     * @param firstname
+     * @param lastname
+     * @return
+     */
+    public int insertRow(long id, String email, String firstname, String lastname){
+        final String INSERT = "INSERT INTO customer_profile  VALUES (:id, :email, :firstname,:lastname)";
+        Map<String, Object> namedParameters = new HashMap<String, Object>();
+        namedParameters.put("id", id);
+        namedParameters.put("email",email);
+        namedParameters.put("firstname",firstname);
+        namedParameters.put("lastname",lastname);
+        getNamedParameterJdbcTemplate().update(INSERT,namedParameters);
+        return 1;
+    }
+
+    public List<CustomerProfile> selectAll(){
+        final String SELECT = "select customer_profile_id, email from customer_profile";
+
+        List<CustomerProfile> profiles = getNamedParameterJdbcTemplate().query(SELECT,(rs,rowNum)->{
+            String email = rs.getString("email");
+            Long id = rs.getLong("customer_profile_id");
+
+            CustomerProfile customerProfile = new CustomerProfile();
+            customerProfile.setEmail(email);
+            customerProfile.setId(id);
+
+            return  customerProfile;
+        });
+
+        return profiles;
+    }
     public static void main(String[] args) {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.load("spring-persistence.xml");
---
+++ src/main/test/com/spring/persistence/dao/CustomerProfileDaoTest.java
@@ -0,0 +1,52 @@
+package com.spring.persistence.dao;
+
+import com.spring.persistence.domain.CustomerProfile;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+/**
+ * Created by sjchen on 8/23/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-persistence.xml"})
+public class CustomerProfileDaoTest extends TestCase {
+
+    @Autowired
+    private CustomerProfileDao customerProfileDao;
+
+    @Test
+    public void testFindEmailNameById() throws Exception {
+        String email = customerProfileDao.findEmailById(1L);
+        System.out.println(email);
+        Assert.notNull(email);
+    }
+    @Test
+    public void testCustomerProfile() {
+
+        CustomerProfile profile = customerProfileDao.customerProfile(1L);
+        Assert.notNull(profile);
+    }
+
+    @Test
+    public void testInsertRow(){
+        //int r = customerProfileDao.insertRow(2,"chensiji@test.com","siji", "chen");
+        CustomerProfile profile = customerProfileDao.customerProfile(2L);
+        //Assert.notNull(profile);
+    }
+
+    @Test
+    public void testSelectAll(){
+        List<CustomerProfile> profiles = customerProfileDao.selectAll();
+        System.out.println(profiles);
+        Assert.notNull(profiles);
+
+    }
+
+}
